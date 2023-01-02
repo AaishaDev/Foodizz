81af74fbd2c929a60343dac6cdc0430dfb7ca8d6
AaishaDev <aaishllap@gmail.com>
2023-01-02 22:36:57 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+ const hamIcon = document.querySelector(".ham_icon");
+ const close = document.querySelector(".close");
+ const menu = document.querySelector('.menu');
+ hamIcon.onclick=()=>menu.style.display = "initial";
+ close.onclick=()=>menu.style.display = "none";
+
+
